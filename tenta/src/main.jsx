6a3ac6b43835607937bb58edf11497937cbc1e24
max <mrivero1992@gmail.com>
2023-12-02 13:33:55 -0300

    button size

--- tenta/src/main.jsx
+++ tenta/src/main.jsx
@@ -15,7 +15,7 @@ function Main() {
           </a>
         </div>
         <button
-          className="px-4 py-1 text-sm text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          className="px-1.5em py-2em text-lg text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
           onClick={() => window.location.reload(true)}
         >
           NUEVO CODIGO
