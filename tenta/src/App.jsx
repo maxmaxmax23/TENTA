6f1cc7b5fbfe540e71872edd659c6b98b932006a
max <mrivero1992@gmail.com>
2023-12-02 13:40:04 -0300

    popover panel

--- tenta/src/App.jsx
+++ tenta/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
       <Popover className="relative">
        <Popover.Button className="px-1.5em py-2em text-lg text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">{scanResult}</Popover.Button>
 
-        <Popover.Panel className="justify-content-center rounded-lg shadow-lg ring-1 ring-black/5 absolute z-10">
+        <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
           <div className="grid-flow-col grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           <div className="justify-self-center mt-1 text-xl font-semibold uppercase leading-tight truncate">
             {Lista.filter((precio) => precio.id === scanResult).map((precio) => (
