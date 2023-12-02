5079d57ebb0a6232b95bbaf4e4b2fc93c74028aa
max <mrivero1992@gmail.com>
2023-12-02 13:21:08 -0300

    flex box for the logo, responsive logo class

--- tenta/src/main.jsx
+++ tenta/src/main.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
-import tentaLogo from './assets/tenta.svg'
-import tentalogov2 from './assets/tentalogov2.svg'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './App.css';
+import tentaLogo from './assets/tenta.svg';
+import tentalogov2 from './assets/tentalogov2.svg';
 
-export default function Main() {  
-
-}
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>    
+function Main() {
+  return (
+    <React.StrictMode>
       <>
-        <div>
-        <a href="https://instagram.com/tentacionesdeco" target="_blank">
-          <img src={tentalogov2} className="logo" alt="logo" />
-        </a>
-      </div>
-      <button className="px-4 py-1 text-sm text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" onClick={() => window.location.reload(true)}>NUEVO CODIGO</button>
-</>
-    
-   <App />
-  </React.StrictMode>,
-)
+        <div className="flex justify-center items-center">
+          <a href="https://instagram.com/tentacionesdeco" target="_blank">
+            <img src={tentalogov2} className="logo" alt="logo" />
+          </a>
+        </div>
+        <button
+          className="px-4 py-1 text-sm text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          onClick={() => window.location.reload(true)}
+        >
+          NUEVO CODIGO
+        </button>
+      </>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
