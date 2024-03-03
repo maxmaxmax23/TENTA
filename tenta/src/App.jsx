a533acd49040dff76bc9b0fb0f1645a24def5727
maxmaxmax <mrivero1992@gmail.com>
2024-03-03 15:17:08 -0300

    NO MATCH, NPM I

--- tenta/src/App.jsx
+++ tenta/src/App.jsx
@@ -40,32 +40,38 @@ function App() {
    <> 
    
    <div className='App'>
-  {scanResult ? (
-      <div>
-      CODIGO:
-      <Popover className="relative">
-       <Popover.Button className="px-1.5em py-2em text-lg text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">{scanResult}</Popover.Button>
+   {scanResult ? (
+  <div>
+    CODIGO:
+    <Popover className="relative">
+      <Popover.Button className="px-1.5em py-2em text-lg text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+        {scanResult}
+      </Popover.Button>
 
-        <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+      <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-3xl">
+        {Lista.filter((precio) => precio.id === scanResult).length > 0 ? (
           <div className="grid-flow-col grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <div className="justify-self-center mt-1 text-xl font-semibold uppercase leading-tight truncate">
-            {Lista.filter((precio) => precio.id === scanResult).map((precio) => (
-              <div key={precio.precio}>${precio.precio}</div>
-            ))}
+            <div className="justify-self-center mt-1 text-xl font-semibold uppercase leading-tight truncate">
+              {Lista.filter((precio) => precio.id === scanResult).map((precio) => (
+                <div key={precio.precio}>${precio.precio}</div>
+              ))}
+            </div>
+            <div className="justify-self-center mt-1 text-xl font-semibold uppercase leading-tight truncate">
+              {Lista.filter((precio) => precio.id === scanResult).map((precio) => (
+                <div key={precio.descripcion}>{precio.descripcion}</div>
+              ))}
+            </div>
           </div>
-          <div className="justify-self-center mt-1 text-xl font-semibold uppercase leading-tight truncate">
-            {Lista.filter((precio) => precio.id === scanResult).map((precio) => (
-              <div key={precio.descripcion}>{precio.descripcion}</div>
-            ))}
-          </div>
-          </div>          
+        ) : (
+          <div>No hay datos para {scanResult}. Consultar en CAJA</div>
+        )}
+      </Popover.Panel>
+    </Popover>
+  </div>
+) : (
+  <div id="reader"></div>
+)}
 
-        </Popover.Panel>
-      </Popover>
-      </div>
-  ) : (
-    <div id="reader"></div>
-  )}
 </div>
  
     </>  
