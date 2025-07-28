c887f6eac6f265e75ad549dfd61ad6455c78641b
maxmaxmax23 <151599785+maxmaxmax23@users.noreply.github.com>
2025-07-28 14:59:17 -0300

    Update main.jsx

--- tenta/src/main.jsx
+++ tenta/src/main.jsx
@@ -17,7 +17,7 @@ function Main() {
           </a>
         </div>
         <button
-          className="px-1.5em py-2em text-lg text-logo-shaddow font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-pink-shaddow hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          className="px-1.5em py-2em text-lg bg-white text-black font-semibold rounded-full border border-black active:text-white active:bg-black active:border-blue-600"
           onClick={() => window.location.reload(true)}
         >
           NUEVO CODIGO
