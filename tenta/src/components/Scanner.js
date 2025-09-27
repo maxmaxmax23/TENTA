737e2336ccae68f30236076c07c21377ec68fa38
maxmaxmax23 <151599785+maxmaxmax23@users.noreply.github.com>
2025-09-27 17:11:27 -0300

    Create Scanner.js

---
+++ tenta/src/components/Scanner.js
@@ -0,0 +1,28 @@
+// src/components/Scanner.js
+import { useEffect } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+function Scanner({ onScan }) {
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: { width: 250, height: 250 },
+      fps: 10,
+      aspectRatio: 2,
+      focusMode: "continuous",
+    });
+
+    scanner.render(
+      (result) => {
+        scanner.clear();
+        onScan(String(result)); // send scanned code to parent
+      },
+      (err) => {
+        console.warn(err);
+      }
+    );
+  }, [onScan]);
+
+  return <div id="reader"></div>;
+}
+
+export default Scanner;
