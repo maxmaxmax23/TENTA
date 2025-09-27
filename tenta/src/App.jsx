3d1364a474901495626647d79d9c0c030aa88330
maxmaxmax23 <151599785+maxmaxmax23@users.noreply.github.com>
2025-09-27 17:48:06 -0300

    Update App.jsx

--- tenta/src/App.jsx
+++ tenta/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { Popover } from "@headlessui/react";
-import Login from "./components/Login";
-import Scanner from "./components/Scanner";
-import ProductUploader from "./components/ProductUploader";
+import Login from "./components/Login.jsx";
+import ProductUploader from "./components/ProductUploader.jsx";
+import Scanner from "./components/Scanner.jsx";
 import Lista from "./tentadb.json"; // Vite supports JSON imports
 
 function App() {
