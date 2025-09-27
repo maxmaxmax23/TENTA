4f99c27347d5c8dbd72f0da5871742fbfce132ad
maxmaxmax23 <151599785+maxmaxmax23@users.noreply.github.com>
2025-09-27 17:34:43 -0300

    Update firebase.js login

--- tenta/src/firebase.js
+++ tenta/src/firebase.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBRTvATe5LG7LNb05AqTTYpAV53Y6B6td0",
+  authDomain: "glowupbb-7b7bc.firebaseapp.com",
+  projectId: "glowupbb-7b7bc",
+  storageBucket: "glowupbb-7b7bc.firebasestorage.app",
+  messagingSenderId: "744754651075",
+  appId: "1:744754651075:web:4b7052c6effcbbe384e776",
+  measurementId: "G-84S8472RN2"
 };
 
 const app = initializeApp(firebaseConfig);
